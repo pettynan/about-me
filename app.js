fc011970c659bf105e5b4b92d39ef3acdfc4ceae
Peter Tynan <pettynan@amazon.com>
2019-03-26 16:04:46 -0700

    some final updates to the JS arrangement. First pass through on html and CSS.

--- app.js
+++ app.js
@@ -13,51 +13,38 @@ var userResponse = []; // This array will be filled with the user's responses to
 
 var yesList = ['yes', 'Yes', 'YES', 'y', 'Y', 'true', 'True', 't', 'T']; // These are all responses accepted for 'true.'
 var noList = ['no', 'No', 'NO', 'n', 'N', 'false', 'False', 'f', 'F']; // These are all responses accepted for 'false.'
+var numCorrect = 0; // Creates a variable which tracks the # of correct answers.
 
-var userName = prompt('Hey, welcome to my about-me! My name is Peter, what should I call you?');
-alert('Nice to meet you, ' + userName + '!');
-
-for (var i = 0; i < 5; i++) {
-  userResponse[i] = prompt(questionArray[i]);
-
-  if ((yesList.includes(userResponse[i]) && answersArray[i] === true ) || (noList.includes(userResponse[i]) && answersArray[i] === false)) { //If the user answered some form of 'yes' and the answer was 'yes', or if the user answered some form of 'no' and the answer was 'no'.
-
-    alert(questionArray[i] + ' You answered ' + userResponse[i] + '. You are correct!');
-    console.log('User answered Question ' + i + ' correctly.');
+setTimeout(function() { // Small delay so that the user can see the main screen before answering prompts. 
+  var userName = prompt('Hey, welcome to my about-me! My name is Peter, what should I call you?');
+  if (userName === 'Evan') {
     
+    alert('Hey Evan! Hope I don\'t need to resubmit the assignment this time D:')
   } else {
-
-    alert(questionArray[i] + ' You answered ' + userResponse[i] + '. You are wrong. :(');
-    console.log('User answered Question ' + i + ' incorrectly.');
-
+  
+  alert('Nice to meet you, ' + userName + '!');
   }
 
-}
+  console.log('User entered their username as \'' + userName + '\'.')
+  alert('Next, I\'m going to ask you a few questions about myself, so you can get to know me.');
 
+  for (var i = 0; i < 5; i++) {
+    userResponse[i] = prompt(questionArray[i]);
 
+    if ((yesList.includes(userResponse[i]) && answersArray[i] === true ) || (noList.includes(userResponse[i]) && answersArray[i] === false)) { //If the user answered some form of 'yes' and the answer was 'yes', or if the user answered some form of 'no' and the answer was 'no'.
 
-  
-// if (userName === 'peter') {
+      numCorrect ++;
 
-//   alert('Hey we\'ve got the same name. Welcome to the site, ' + userName + '!')
+      alert(questionArray[i] + ' You answered ' + userResponse[i] + '. You are correct! You\'ve gotten ' + numCorrect + ' out of ' + (i + 1) + ' questions right.');
+      console.log('User answered Question ' + i + ' correctly.');
+      
+    } else {
 
-// } else {
-//   console.log('Welcome to the site ' + userName);
-// }
+      alert(questionArray[i] + ' You answered ' + userResponse[i] + '. You are wrong :( You\'ve gotten ' + numCorrect + ' out of ' + (i + 1) + ' questions right.');
+      console.log('User answered Question ' + i + ' incorrectly.');
 
+    }
 
-
-
-
-
-
-
-/*
-Arrays
-
-declare an array with "[]"
-put things into an array, when we define it, by typing the values
-[value1, value2] === "array literal"
-elements can be any value in js
-*/
-
+  }
+  console.log('User answered ' + numCorrect + ' out of 5 questions correctly.')
+}, 500);
